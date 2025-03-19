19dd1c2424c7ad675ef5a9780058fa766f648edb
Steve Dmello <steveaarond@gmail.com>
2025-03-19 17:22:18 +0530

    final_final ver

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import os
 
 from flask import Flask,render_template,request
 import sqlite3
@@ -27,52 +26,51 @@ app = Flask(__name__)
 def home():
     infos=db.execute("SELECT * FROM stocks").fetchall()
     print(infos)
-    
+
     return render_template('home.html',infos=infos)
 
 @app.route('/search', methods=['GET', 'POST'])    
 def search():
-    if request.method == 'POST':
-        try:
-            stock=request.form.get('search')
-        except AttributeError or not stock:
-            return render_template('Apology.html',apology='Please enter a valid Company name')
-        stock=stock.lower()
-        url = f"https://newsapi.org/v2/everything?q={stock}&language=en&sortBy=publishedAt&apiKey={APIKEY}"
-        sia=SentimentIntensityAnalyzer()
 
-        subreddit = reddit.subreddit("StockMarket")
-        redtitles = []
-        redurls=[]
-        redcomments=[]
-        i=0
-        for post in subreddit.hot(limit=100):
-            if stock in post.title.lower() and i<4:
-                redtitles.append(post.title)
-                redcomments.append(post.num_comments)
-                redurls.append(post.url)
-                i+=1
+    try:
+        stock=request.form.get('search') 
+        if not stock:
+            stock=request.args.get('search')
+        
+    except AttributeError or not stock:
+        return render_template('Apology.html',apology='Please enter a valid Company name')
+    stock=stock.lower()
+    url = f"https://newsapi.org/v2/everything?q={stock}&language=en&sortBy=publishedAt&apiKey={APIKEY}"
+    sia=SentimentIntensityAnalyzer()
 
-        red_score=sia.polarity_scores(str(redtitles))
-        red_score=red_score['compound']  
+    subreddit = reddit.subreddit("StockMarket")
+    redtitles = []
+    redurls=[]
+    i=0
+    for post in subreddit.hot(limit=100):
+        if stock in post.title.lower() and i<4:
+            redtitles.append(post.title)
+            redurls.append(post.url)
+            i+=1
 
-        news=requests.get(url).json()
-        newsscores=[]
-        newstitles=[]
-        for article in news['articles'][:5]:
-            newstitles.append(article['title'])
-        newsscores=sia.polarity_scores(str(newstitles))
-        newsscores=newsscores['compound']
-        if (newsscores+red_score)/2>0:
-            sentiment="Positive"
-        elif (newsscores+red_score)/2<0:
-            sentiment="Negative"
-        else:
-            sentiment="Neutral"
-        print(sentiment,stock)
+    red_score=sia.polarity_scores(str(redtitles))
+    red_score=red_score['compound']  
 
-        db.execute("INSERT INTO stocks (company,sentiment) VALUES (?,?)",(stock,sentiment)) 
-        conn.commit()             
-        return render_template('search.html',redurls=redurls,redcomments=redcomments,redtitles=redtitles,stock=stock,redscore=red_score,news=news['articles'][:5],newsscore=newsscores,i=i)
+    news=requests.get(url).json()
+    newsscores=[]
+    newstitles=[]
+    for article in news['articles'][:5]:
+        newstitles.append(article['title'])
+    newsscores=sia.polarity_scores(str(newstitles))
+    newsscores=newsscores['compound']
+    if (newsscores+red_score)/2>0:
+        sentiment="Positive"
+    elif (newsscores+red_score)/2<0:
+        sentiment="Negative"
     else:
-        return render_template('Apology.html',apology='GET request not allowed') 
+        sentiment="Neutral"
+    print(sentiment,stock)
+
+    db.execute("INSERT INTO stocks (company,sentiment) VALUES (?,?)",(stock.upper(),sentiment)) 
+    conn.commit()             
+    return render_template('search.html',redurls=redurls,redtitles=redtitles,stock=stock,redscore=red_score,news=news['articles'][:5],newsscore=newsscores,i=i)
